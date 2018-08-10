29d8298ae517d0078d966e40666a772c8d76367f
Matthias Sperber <matthias.sperber@gmail.com>
2018-08-10 17:17:55 +0200

    initial npz_to_h5.py

---
+++ script/custom/npz_to_h5.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+
+import numpy as np
+import h5py
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--input", help="Path to npz file to read from")
+  parser.add_argument("--output", help="Path to h5 to write out")
+  args = parser.parse_args()
+
+
+  npz_file = np.load(args.input, mmap_mode="r")
+  npz_keys = sorted(npz_file.files, key=lambda x: int(x.split('_')[-1]))
+
+  with h5py.File(args.output, "w") as hf:
+
+    for sent_no, key in enumerate(npz_keys):
+      features = npz_file[key]
+      hf.create_dataset(str(key), data=features)
+
+  npz_file.close()
+
+if __name__ == "__main__":
+  sys.exit(main())
