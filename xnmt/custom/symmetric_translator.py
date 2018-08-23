5841c388c50df0347bfba9cec118333aba2c897b
Matthias Sperber <matthias.sperber@gmail.com>
2018-08-23 16:24:41 +0200

    comments

--- xnmt/custom/symmetric_translator.py
+++ xnmt/custom/symmetric_translator.py
@@ -222,6 +222,7 @@ class SymmetricTranslator(models.ConditionedModel, models.GeneratorModel, Serial
             lstm_input2 = dy.dropout_batch(lstm_input2, self.split_dual[1])
           lstm_input = self.split_dual_proj(dy.concatenate([lstm_input,lstm_input2]))
         if self.split_regularizer and pos < len(self._chosen_rnn_inputs):
+          # _chosen_rnn_inputs does not contain first (empty) input, so this is in fact like comparing to pos-1:
           penalty = dy.squared_norm(lstm_input - self._chosen_rnn_inputs[pos])
           if self.split_regularizer != 1:
             penalty = self.split_regularizer * penalty
