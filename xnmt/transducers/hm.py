d0f618da27884dece3f53a4b83e69af0292e68b7
Elizabeth Salesky <elizabeth.salesky@gmail.com>
2018-11-16 15:14:30 -0500

    - gets through one batch, then stale expr error

--- xnmt/transducers/hm.py
+++ xnmt/transducers/hm.py
@@ -18,15 +18,15 @@ from xnmt.persistence import serializable_init, Serializable, Ref, bare
 
 #todo: annealing schedule for a: `annealing the slope of the hard binarizer from 1.0 to 5.0 over 80k minibatches'
 def hard_sigmoid_anneal(zz, a=1.0):
-    print(zz.dim())
-    tmp = ((a * zz) + 1.0) / 2.0
-#    output = np.clip(tmp, a_min=0, a_max=1) #?? can i do this to a dynet object? is there another way?
-    if tmp.value() > 1:
-        return dy.ones(dim=1,)
-    elif tmp.value() < 0:
-        return dy.zeroes(dim=1,)
-    else:
-        return tmp
+    tmp = ((a * zz) + 1.0) / 2.0 #todo: clip to [0,1]
+    return tmp
+##    output = np.clip(tmp, a_min=0, a_max=1) #?? can i do this to a dynet object? is there another way?
+#    if tmp.value() > 1:
+#        return dy.ones(dim=1,)
+#    elif tmp.value() < 0:
+#        return dy.zeroes(dim=1,)
+#    else:
+#        return tmp
 
 class HMLSTMCell(transducers.SeqTransducer, Serializable):
     """
@@ -67,29 +67,24 @@ class HMLSTMCell(transducers.SeqTransducer, Serializable):
     def transduce(self, h_below: 'expression_seqs.ExpressionSequence', h_above, z_below) -> 'expression_seqs.ExpressionSequence':
         if self.c == None:
             self.c = dy.zeroes(dim=(self.hidden_dim,)) #?? does (hidden,) take care of batch_size?
+        if self.h == None:
             self.h = dy.zeroes(dim=(self.hidden_dim,))
+        if self.z == None:
             self.z = dy.ones(dim=(1,))
 
         W_1l_r  = dy.parameter(self.p_W_1l_r)
         bias = dy.parameter(self.p_bias)
-        
-        s_recur = W_1l_r * self.h #matrix multiply is *, element-wise is dy.cmult. CURRERROR: stale expression
+        h = dy.parameter(self.h)
+
+        s_recur = W_1l_r * h #matrix multiply is *, element-wise is dy.cmult. CURRERROR: stale expression
         if not self.last_layer:
             W_2l_td = dy.parameter(self.p_W_2l_td)
             W_0l_bu = dy.parameter(self.p_W_0l_bu)
-#            print(W_0l_bu.dim())
-#            print(h_below.dim())
             s_bottomup = W_0l_bu * h_below #?? this is becoming (2049,). does it need to be (2049,1) to do scalar * matrix?
-#            print(s_bottomup.dim())
-#            print(z_below.dim())
-#            print(self.last_layer)
             s_topdown  = W_2l_td * h_above
         else:
-            s_topdown  = dy.zeroes(s_recur.dim(),) #?? this gets the shape e.g. ((5, 1), 1). do i actually want batch_size as well?
-            s_bottomup = W_1l_r * self.h
-#        zb_expand = z_below * dy.ones(dim=(z_below.dim()[1],self.hidden_dim*4+1)) #?? batch size handled? this should output z tiled to batch_size
-#        print(zb_expand.dim())
-#        s_bottomup = dy.mult(zb_expand, s_bottomup)
+            s_topdown  = dy.zeroes(s_recur.dim()[0][0],) #?? this gets the shape e.g. ((5, 1), 1). do i actually want batch_size as well?
+            s_bottomup = W_1l_r * h
         s_bottomup = dy.cmult(z_below,s_bottomup) #to handle batched scalar * matrix -> e.g. (1x10, 2049x10)
         s_topdown  = dy.cmult(self.z,s_topdown)  #will be zeros if last_layer. is this right, or should z=1 in this case ??
         
@@ -105,8 +100,6 @@ class HMLSTMCell(transducers.SeqTransducer, Serializable):
         g_t = dy.tanh(i_gt)
 
         z_tmp = dy.pick_range(fslice, self.hidden_dim*4,self.hidden_dim*4+1)
-        print(z_tmp.dim())
-        print("--")
         z_tilde = hard_sigmoid_anneal(z_tmp,self.a)  #should be hard sigmoid + slope annealing
         z_new = dy.round(z_tilde, gradient_mode="straight_through_gradient")  #use straight-through estimator for gradient: step fn forward, hard sigmoid backward
 
@@ -119,7 +112,7 @@ class HMLSTMCell(transducers.SeqTransducer, Serializable):
             c_new = self.c
             h_new = self.h
         else: #UPDATE
-            c_new = dy.cmult(f_t, self.c) + dy.cmult(i_t * g_t)
+            c_new = dy.cmult(f_t, self.c) + dy.cmult(i_t, g_t)
             h_new = dy.cmult(o_t, dy.tanh(c_new))
         
         self.c = c_new
@@ -169,7 +162,6 @@ class HM_LSTMTransducer(transducers.SeqTransducer, Serializable):
     def transduce(self, xs: 'expression_seqs.ExpressionSequence') -> 'expression_seqs.ExpressionSequence':  
         
         batch_size = xs[0][0].dim()[1]
-        print("BATCH_SIZE %d" % batch_size)
         h_bot = []
         h_top = []
         z_bot = []
@@ -179,9 +171,6 @@ class HM_LSTMTransducer(transducers.SeqTransducer, Serializable):
         z_one   = dy.ones(1, batch_size=batch_size)
         h_bot.append(dy.zeroes(dim=(self.hidden_dim,), batch_size=batch_size)) #indices for timesteps are +1
         h_top.append(dy.zeroes(dim=(self.hidden_dim,), batch_size=batch_size))
-#        z_one   = dy.ones(dim=(1,))
-#        h_bot.append(dy.zeroes(dim=(self.hidden_dim,),))
-#        h_top.append(dy.zeroes(dim=(self.hidden_dim,),))
         
         for i, x_t in enumerate(xs):
             h_t_bot, z_t_bot = self.bottom_layer.transduce(h_below=x_t, h_above=h_top[i], z_below=z_one) #uses h_t_top from layer above@previous time step, h_t_bot and z_t_bot from previous time step (saved in hmlstmcell)
@@ -194,4 +183,5 @@ class HM_LSTMTransducer(transducers.SeqTransducer, Serializable):
 
         #final state is last hidden state from top layer
         self._final_states = [transducers.FinalTransducerState(h_top[-1])]
-        return h_top[1:] #removes the init zeros to make it same length as seq
+        fin_xs = expression_seqs.ExpressionSequence(expr_list=h_top[1:])
+        return fin_xs #removes the init zeros to make it same length as seq
